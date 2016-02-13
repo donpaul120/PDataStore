f6481a9531e303e596f2edf120948f076f5543b6
Okeke Paul Ugochukwu <paulex@localhost.localdomain>
2016-02-13 16:59:13 +0000

    added test

---
+++ test/test.js
@@ -0,0 +1,39 @@
+
+var pStore = new PDataStore();
+
+
+
+describe('PDataStore', function(){
+
+    describe('#retrieveData', function(){
+        it("Should return null if the data-source doesn't exist <--> ", function(){
+            var data = pStore.retrieveData("data-mole");
+            assert(null === data, 'The result returned from retrieveData isnt null');
+        });
+    });
+
+
+    describe('#findDataByKey', function(){
+        it('should return an empty array if the data source doesnt exist -- ', function(){
+            var result = pStore.findDataByKeyValue('s', 'b', 'data-source-no-exist').result;
+            assert([].length === result.length, 'the result is not empty');
+        });
+    });
+
+    describe('#findDataByKey on a Wrong Data Source Format', function(){
+        it('should throw Error if the data source format isn;t in a json or url format-- ', function(){
+            var result;
+            try{
+               result = pStore.findDataByKeyValue('k', 'v', 'data-wrong-format');
+            }catch (e){
+                if(e){
+                    assert(e instanceof Error)
+                }
+            }
+            if(result){
+                assert(false);
+            }
+        });
+    })
+
+});
